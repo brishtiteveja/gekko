4d6ed9619429bd759c98d7a98cc26896f4871d5e
Zehady Abdullah Khan <brishtiteveja@gmail.com>
2019-04-09 00:39:19 +0000

    Multiple config file generator code for multiple pair added.

---
+++ multi-config-generator/index.js
@@ -0,0 +1,6 @@
+const gekkoPath = require('./src/isWindows.js')
+    ? 'C:\\path\\to\\gekko\\gekko'
+    : "/home/ubuntu/gekko";
+
+const generator = require("./src/generator");
+generator.run(gekkoPath);
